165678f92a5cf9ef30343a00c51b40d120411b2d
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-03-30 10:12:58 -0300

    test: add OptionSerializer test

---
+++ api/spec/serializers/option_serializer_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'OptionServialize' do
+  before(:each) do
+    @option = Option.new({ description: 'any', poll_id: 1})
+    @serializer = OptionSerializer.new(@option)
+    @serialization = ActiveModelSerializers::Adapter.create(@serializer)
+  end
+
+  subject { JSON.parse(@serialization.to_json) }
+
+  it 'should have a description that matches' do
+    expect(subject['description']).to eql(@option.description)
+  end
+end
