4a38882ffd3085b8087960dd8c6cbde9a2accc59
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-03-31 17:42:27 -0300

    fix: guardian error

--- api/config/initializers/devise.rb
+++ api/config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'ce8cb61d3a0128139c27ba31b78b7030cfabddc318fbcbd57078efc5d7ad7f61a8ad857c47d8e66b24258f19e9c0202c9a21b65af3cf117cea664aebc1c8a542'
+  # config.secret_key = ENV('DEVISE_SECRET_KEY')
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
