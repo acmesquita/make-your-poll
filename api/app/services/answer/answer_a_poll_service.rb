da30da8968f44e19bfde17bc88d470f862f0195c
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-03-31 12:23:04 -0300

    fix: change params to AnswerAPollService

--- api/app/services/answer/answer_a_poll_service.rb
+++ api/app/services/answer/answer_a_poll_service.rb
@@ -1,9 +1,9 @@
 class Answer::AnswerAPollService
 
-  def self.call(poll_id, option_id)
+  def self.call(params)
     answer = Answer.create({
-      poll_id: poll_id,
-      option_id: option_id
+      poll_id: params[:poll_id],
+      option_id: params[:option_id]
     })
   end
 end
--- api/spec/services/answer/answer_a_poll_service_spec.rb
+++ api/spec/services/answer/answer_a_poll_service_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'AnswerAPollService' do
         options: [ { :description => "any_1" } ]
       }
       poll = Poll::CreatePollService.call(params)
-      answer = Answer::AnswerAPollService.call(poll.id, poll.options.first.id)
+      answer = Answer::AnswerAPollService.call({ poll_id: poll.id, option_id: poll.options.first.id })
 
       expect(answer).to be_valid
 
@@ -17,7 +17,7 @@ RSpec.describe 'AnswerAPollService' do
   end
   describe 'should be return poll with errors if provider incorrectly params' do
     it 'when poll id and option id  is invalid' do
-      answer = Answer::AnswerAPollService.call(nil, nil)
+      answer = Answer::AnswerAPollService.call({})
 
       expect(answer).to_not be_valid
       expect(answer.errors[:poll].first).to eq('must exist')
