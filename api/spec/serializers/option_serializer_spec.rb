4b373070007962e20811d76e0cc5356ee20df8f3
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-04-02 19:41:25 -0300

    test: fix tests option serializer

--- api/spec/serializers/option_serializer_spec.rb
+++ api/spec/serializers/option_serializer_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'OptionSerializer' do
   before(:each) do
-    @option = Option.new({ description: 'any', poll_id: 1})
+    user = User.create({username: 'xpto', password: '12345678'})
+    poll = Poll.create({
+      title: 'What is the best food in the world?',
+      description: 'From the options below, choose what would be your favorite food.',
+      user: user
+    })
+    @option = Option.new({ description: 'any', poll_id: poll.id})
     @serializer = OptionSerializer.new(@option)
     @serialization = ActiveModelSerializers::Adapter.create(@serializer)
   end
