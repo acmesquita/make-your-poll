1954ad5e08f871960aa7226e65dd003e05e124a1
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-04-02 19:32:27 -0300

    test: fix tests find a poll service

--- api/spec/services/poll/find_poll_service_spec.rb
+++ api/spec/services/poll/find_poll_service_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe 'FindPollService' do
   describe 'should be return a poll in database' do
     it 'when provider id valid' do
+      user = User.create({username: 'xpto', password: '12345678'})
       params = {
         title: Faker::Lorem.words(number: 4, supplemental: true).join(' '),
         description: Faker::Lorem.words(number: 4, supplemental: true).join(' '),
+        user_id: user.id,
         options: [ { :description => "any_1" } ]
       }
       pollInsert = Poll::CreatePollService.call(params)
